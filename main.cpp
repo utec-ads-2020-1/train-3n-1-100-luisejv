db5619d9e4b87e177bddda2597e7ac2633d5b0f9
luisejv <ljauregui@pucp.pe>
2020-03-31 18:02:30 -0500

    work done

--- main.cpp
+++ main.cpp
@@ -1,2 +1,41 @@
-int main(){
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	fstream *arch = new fstream("prueba.txt", ios_base::in);
+	if (!(arch->is_open()))
+	{
+		cout << "Archivo fallo en leer" << endl;
 	}
+	else
+	{
+		string linea;
+		while (getline(*arch, linea))
+		{
+			stringstream stream(linea);
+			string i, j;
+			int num1, num2, mayorCiclo = 0;
+			getline(stream, i, ' ');
+			getline(stream, j, ' ');
+			num1 = stoi(i);
+			num2 = stoi(j);
+			for (num1; num1 <= num2; num1++)
+			{
+				int finalNum = num1;
+				int contador = 1;
+				while (finalNum != 1)
+				{
+					finalNum = (finalNum % 2 == 0) ? finalNum / 2 : 3 * finalNum + 1;
+					contador++;
+				}
+				mayorCiclo = (mayorCiclo < contador) ? contador : mayorCiclo;
+			}
+			cout << i << " " << j << " " << mayorCiclo << endl;
+		}
+	}
+}
